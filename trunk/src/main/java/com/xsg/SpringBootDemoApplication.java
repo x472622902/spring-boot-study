e50ec68e12c682912971b118bb1cc132144ad0bd
xsg <472622902@qq.com>
2016-10-21 15:33:14 +0800

    first commit

--- trunk/src/main/java/com/xsg/SpringBootDemoApplication.java
+++ trunk/src/main/java/com/xsg/SpringBootDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
+		//main application
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 }
